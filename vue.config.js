d8ab253d841ab1c368a024840d76f555c3c5c6d2
shestmintsev-kirill <1kirillmihail1@gmail.com>
2021-11-24 13:43:09 +0300

    router hash

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,7 @@ function resolve(dir) {
 }
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/proto-taxi/' : '/',
   configureWebpack: {
     context: resolve(''),
     devServer: {
